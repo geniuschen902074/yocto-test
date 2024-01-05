d8f46879c1a8f0da054969c93b4ffdfefd2fe4d7
jacob.chen <jacob.chen@insyde.com>
2024-01-05 10:24:08 +0800

    test

--- bbexample.c
+++ bbexample.c
@@ -5,6 +5,7 @@
 
 int main(int argc, char *argv[])
 {
+/*
   printf("secret: [");
   FILE *fp = fopen("secret.txt", "r");
   char s[64];
@@ -12,6 +13,8 @@ int main(int argc, char *argv[])
   printf("%s]\n", s);
 
   fclose(fp);
+*/
+  printf("hello bb\n");
 
   return 0;
 }
